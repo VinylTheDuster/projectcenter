27a3ee20113095f86b2ec904e362a1892d99bbfe
Vinyl <38420904+VinylTheDuster@users.noreply.github.com>
2025-08-30 18:16:37 +0200

    Added multiple features and debugging => ability to delete a card using the cross in the top right corner of the card.

--- src/pages/bettertodolist/scripts/bettertodolistlogic.js
+++ src/pages/bettertodolist/scripts/bettertodolistlogic.js
@@ -3,20 +3,24 @@
 
 const cardContainer = document.getElementById('card-container');
 
-//addCard
+//add/delCard
 const cardBtnSubmit = document.getElementById('addCard-submit');
 
-//add/deleteCat
+//add/deleteCat/Card
 const catBtnSubmit = document.getElementById('addCat-submit');
 const delCatBtnSubmit = document.getElementById('delCat-submit');
+const delCardBtnSubmit = document.getElementById('delCard-submit');
 
+let currentCardData = getContentFromLocalStorage();
 let currentCategory = [];
 
-// => functions
+let currentCardIterations = [];
+
+// => classes & functions
 
 class cardTemplate {
 
-    constructor(id, title, category, description, priority, done = false, date = new Date()) {
+    constructor(id = 0, title, category, description, priority, done = false, date = new Date()) {
 
         this.id = id;
         this.title = title;
@@ -28,26 +32,50 @@ class cardTemplate {
     }
 }
 
-function getContentFromLocalStorage() { 
+function getContentFromLocalStorage() {
+
+    let data = {};
+
+    for (let i = 0; i < localStorage.length; i++) {
+
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+
+        data[key] = value;
+    }
     
-    let _localStorageArr = []; 
-    
-    for (let i = 0; i < localStorage.length; i++) { 
-        
-        let key = localStorage.key(i); 
-        let value = localStorage.getItem(key); 
-        
-        _localStorageArr.push( key, value );
-    } 
-    
-    return _localStorageArr; 
+    return data;
 }
 
 function getCategoryFromLocalStorage() {
 
+    for (let i = 0; i < localStorage.length; i++) {
+
+        let storedData = localStorage.getItem(i);
+        let inputedCategory = JSON.parse(storedData).category;
+
+        if (!currentCategory.includes(inputedCategory)) {
+            currentCategory.push(JSON.parse(storedData).category);
+        }
+    }
+
+}
+
+function updateLocalStorage() {
+    for (let i = 0; i < Object.keys(currentCardData).length; i++) {
+
+        let key = i;        
+        let value = JSON.stringify(currentCardData[key]);
+        localStorage.setItem(key, value);
+    }
+
+    console.log(localStorage);
+    initializeCardContainer();
 }
 
 function createNewCard(title, category, description, priority) {
+
+    let id = (Object.keys(currentCardData).length === 0 ? 0 : Object.keys(currentCardData).length);
     
     if (!category) {
         
@@ -59,12 +87,22 @@ function createNewCard(title, category, description, priority) {
         return alert('Veuillez choisir une priorité !');
     }
 
-    return new cardTemplate(title, category, description, priority);
+    let newCard = new cardTemplate(id, title, category, description, priority);
+
+    return addCardToVariables(newCard);
+}
+
+function addCardToVariables(cardToAdd) {
+
+    let dataKey = cardToAdd.id;
+    
+    currentCardData[dataKey] = cardToAdd;
+    updateLocalStorage();
 }
 
 // categories manipulation functions
 
-function addNewCategory() {
+function addNewCategory(category) {
 
     const catTitle = document.getElementById('addCat-title');
 
@@ -82,7 +120,19 @@ function deleteCategory() {
 
     const delCatTitle = document.getElementById('delCat-title').value;
 
-    if (delCatTitle) {
+    let isDeletable = () => {
+
+        for (let i = 0; i < localStorage.length; i++) {
+
+            let category = JSON.stringify(currentCardData[i]).category;
+
+            if (category == delCatTitle) {
+                return true;
+            }
+        }
+    }
+
+    if (delCatTitle && !isDeletable) {
         currentCategory.pop(delCatTitle);
         return initializeCategoryFields();
     }
@@ -93,10 +143,11 @@ function deleteCategory() {
 function initializeCategoryFields() {
     
     const categorySelect = document.querySelectorAll('.option-category');
+    getCategoryFromLocalStorage();
 
     categorySelect.forEach((x) => {
-        x.innerHTML = ``
-        x.innerHTML += `<option value="">--Choisissez une catégorie--</option>`
+
+        x.innerHTML = `<option value="">--Choisissez une catégorie--</option>`
 
         if (currentCategory && currentCategory.length > 0) {
             for (let i = 0; i < currentCategory.length; i++) {
@@ -108,21 +159,104 @@ function initializeCategoryFields() {
     });
 }
 
-initializeCategoryFields();
+// card rendering
+
+function initializeCardContainer() {
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = "";
+
+    let dataFromStorage = getContentFromLocalStorage();
+
+    let hasLooped = false;
+
+    for (key in dataFromStorage) {
+
+        let rawData = dataFromStorage[key];
+        let data = typeof rawData === "string" ? JSON.parse(rawData) : rawData;
+
+        console.log('ça boucle');
+        cardContainer.innerHTML += `
+        <div id="${'card' + data.id}" class="card-item">
+            <h3>${data.title}</h3>
+            <span id="${'span' + data.id} class="delCard">╳</span>
+            <div class="card-info">
+                <div class="card-info-labels">
+                    <h4>Catégorie :</h4><p>${data.category}</p>
+                    <h4>Prioritée :</h4><p>${data.priority}</p>
+                </div>
+                <div class="card-info-description">
+                    <h4>Description :</h4>
+                    <p>${data.description}</p>
+                </div>
+            </div>
+            <div class="card-info-date">
+                <p>${data.date}</p>
+            </div>
+            <div class="card-btn-done">
+                <button>Clôturer la tâche</button>
+            </div>
+        </div>`;
+
+        hasLooped = true;
+
+        // initialize events listeners for deleting card with the span X
+        let currentSpan = document.getElementById('span' + data.id);
+
+        currentSpan.addEventListener('click'), function () {
+
+            localStorage.removeItem(key);
+            this.parentNode.remove();
+
+            //function to reorganize localstorage
+
+            getContentFromLocalStorage();
+        };
+    }
+    
+    if (!hasLooped) {
+        cardContainer.innerHTML = ``;
+        console.log('hasntloop');
+    }
+
+    initializeCategoryFields();
+}
+
+// options
+
+function deleteAllCard() {
+
+    localStorage.clear();
+    currentCategory = [];
+    
+    initializeCardContainer();
+    initializeCategoryFields();
+}
 
 // => actions & eventListeners
 
+initializeCategoryFields();
+initializeCardContainer();
+
 console.log(getContentFromLocalStorage());
+
+function addAllEvents () {
+    
+}
 
 cardBtnSubmit.addEventListener('click', () => { 
 
     const cardTitle = document.getElementById('addCard-title').value;
     const cardDesc = document.getElementById('addCard-desc').value;
-    const cardCategory = document.getElementById('addCard-category').value;
-    const cardPriority = document.getElementById('addCard-priority').value;
+    const cardCategory = document.getElementById('addCard-category');
+    const cardPriority = document.getElementById('addCard-priority');
 
-    createNewCard(cardTitle, cardCategory, cardDesc, cardPriority) 
+    let cardCatText = cardCategory.options[cardCategory.selectedIndex].textContent;
+    let cardPriText = cardPriority.options[cardPriority.selectedIndex].textContent;
+
+    createNewCard(cardTitle, cardCatText, cardDesc, cardPriText); 
 });
 
 catBtnSubmit.addEventListener('click', () => addNewCategory());
 delCatBtnSubmit.addEventListener('click', () => deleteCategory());
+delCardBtnSubmit.addEventListener('click', () => deleteAllCard());
